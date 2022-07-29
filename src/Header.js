40622076cacf060eb54d9060d993e43d0aa84cc9
jaydelim <jaydelim89@gmail.com>
2022-07-29 15:14:35 -0400

    fix: modify mobile layout in Header component

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (
-      <div className="bg-[#5cb85c] text-white flex items-center justify-center h-48 w-full min-w-[640px]">
+      <div className="bg-[#5cb85c] text-white flex items-center justify-center h-48 w-full min-w-[456px]">
         <div>
           <p className="text-6xl font-medium text-center py-4">conduit</p>
           <p className="text-2xl font-extralight ">A place to share your knowledge.</p>
