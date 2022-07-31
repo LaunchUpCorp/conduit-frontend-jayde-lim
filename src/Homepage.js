e7fa7d008a454d7c27f35beee8cc8bfdbf6427a9
jaydelim <jaydelim89@gmail.com>
2022-07-31 15:30:15 -0400

    refactor: create Homepage component and restructure

---
+++ src/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Body from './Body';
+
+function Homepage() {
+  return (
+    <>
+      <Header/>
+      <Body/>
+    </>
+    )
+}
+
+export default Homepage;
